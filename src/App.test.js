b4f93d0a493a1020098603c056872ac033fbdac9
Neel Sandell <sandellfamily2013@gmail.com>
2022-01-10 01:39:27 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('wagie', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Add New Element');
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <button>Add New List Item</button>;
+};
+
+export default Button;
